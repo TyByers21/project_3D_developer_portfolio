999fae40312aaa9bac9ea4f37483c37e48aee206
wraphx <tyrone@wraphx.com>
2024-11-10 15:53:35 -0500

    Looks Great

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
-import { motion } from "framer-motion";
+
 import 'boxicons';
+import { ReactTyped } from "react-typed";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
@@ -9,54 +10,51 @@ const Hero = () => {
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
-        
-
         <div className="home-content">
           <h1 className={`${styles.heroHeadText} orange-text-gradient`}>
-            Ty Byers <span className='text-[#915EFF]'></span>
+            Ty Byers 
           </h1>
-          <h3 className="text-animation">I'm a <span></span></h3>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          Step into a world where technology meets artistry, and innovation unfolds through code and motion. Turning ideas into visually stunning products is my passion. <br className='sm:block hidden' />
-            interfaces and web applications
-          </p>
-          <div className="social-icons">
-            <a href="#"><box-icon name='linkedin-square' type='logo' color='#ffffff' ></box-icon></a>
-            <a href="#"><box-icon type='logo' name='github'></box-icon></a>
-            <a href="#"><box-icon type='logo' name='instagram-alt'></box-icon></a>
-            
-           
-          </div>
-          <div className="btn-group">
-            <a href="#" className="btn">Hire</a>
-            <a href="#" className="btn">Contact</a>
-          </div>
-          </div>
-          <div className="home-img">
-            <img src="avatar2.png" alt="" />
+          <h3 className="">I'm a <span className="orange-text-gradient"> 
+            <ReactTyped
+              strings={[
+                "Designer...",
+                "Developer...",
+                "Dreamer...",
+              ]}
+              typeSpeed={40}
+              backSpeed={50}
+              loop
+               >
+              
+            </ReactTyped>
+            </span>
+
+          </h3>
+          <p className='mt-2 text-white-200'>
+            Step into a world where technology meets artistry, and innovation unfolds through code and motion. Turning ideas into visually stunning products is my passion. <br className='sm:block hidden' />
           
+          </p>
+
+          <div className="btn-group mt-5">
+            <a href="#projects" className="btn">View My Portfolio</a>
+            <a href="#contact" className="btn">Contact</a>
+          </div>
+          <div className="social-icons">
+            <a className="ml-44" href="#"><box-icon name='linkedin-square' type='logo' color='#000000' ></box-icon></a>
+            <a className="ml-10" href="#"><box-icon type='logo' name='github'></box-icon></a>
+            <a className="ml-10" href="#"><box-icon type='logo' name='instagram-alt'></box-icon></a>
+
+
+          </div>
+        </div>
+        <div className="home-img">
+          <img src="avatar2.png" alt="" />
         </div>
       </div>
 
       {/* <ComputersCanvas /> */}
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
-          </div>
-        </a>
-      </div>
+
     </section>
   );
 };
