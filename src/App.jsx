5a87a10f07c1394f0ecd2c16fb292db5483e0d49
wraphx <tybyers21@gmail.com>
2024-11-18 20:34:15 -0500

    Added 3D Projects Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import Projects from "./components/Projects";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
+        <Projects />
         <Feedbacks />
         <div className='relative z-0'>
           <Contact />
